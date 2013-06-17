f208a0c69ff344455e11d63bac0467d3f3fa4aa9
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2013-06-17 20:57:51 +0200

    Interface + Closing

--- req.go
+++ req.go
@@ -7,6 +7,40 @@ import (
     "fmt";
 )
 
+type requests struct {
+    uri string
+    concurrency int
+    results []Result
+    closing chan chan error
+}
+
+type Requester interface {
+    Close() error
+    Results() []Result
+}
+
+func ConcurrentRequests(uri string, concurrency int) Requester {
+    r := &requests{
+        uri: uri,
+        concurrency: concurrency,
+        closing: make(chan chan error),
+    }
+
+    go r.loop()
+
+    return r
+}
+
+func (r *requests) Close() error {
+    errc := make(chan error)
+    r.closing <- errc
+    return <-errc
+}
+
+func (r *requests) Results() []Result {
+    return r.results
+}
+
 type Result struct {
     Url           string
     StatusCode    int
@@ -14,9 +48,9 @@ type Result struct {
     ResponseTime  time.Duration
 }
 
-func fetch(url string) Result {
+func (r *requests) fetch() Result {
     t0 := time.Now()
-    resp, err := http.Get(url)
+    resp, err := http.Get(r.uri)
     if err != nil {
         fmt.Println("resp failed")
     }
@@ -24,30 +58,47 @@ func fetch(url string) Result {
 
     ioutil.ReadAll(resp.Body)
 
-    return Result{url, resp.StatusCode, resp.ContentLength, time.Since(t0)}
+    return Result{r.uri, resp.StatusCode, resp.ContentLength, time.Since(t0)}
 }
 
-func concurrentRequests(url string, concurrency int) {
-    c := make(chan bool, concurrency)
-    res := make(chan Result, concurrency)
+
+func (r *requests) loop() {
+    c := make(chan bool, r.concurrency)
+    res := make(chan Result, r.concurrency)
+    var err error
+    open := true
 
     for {
         select {
         case c <- true:
             go func() {
-                res <- fetch(url);
-                <- c
+                result := r.fetch()
+                if open {
+                    res <- result
+                    <- c
+                }
             }()
         case result := <- res:
+            r.results = append(r.results, result)
             fmt.Println(result)
-        case <- time.After(time.Second):
-            fmt.Println("timeout")
+        case errc := <-r.closing:
+            open = false
+            errc <- err
+            close(res)
+            close(c)
+            return
         }
     }
 }
 
 func main() {
-    go concurrentRequests("http://localhost:3000", 10)
+    fetching := ConcurrentRequests("http://localhost:3000", 10)
+
+    // Stop fetching after some time
+    time.AfterFunc(3*time.Second, func() {
+        fetching.Close()
+        fmt.Println("Closed!", len(fetching.Results()))
+    })
 
     var input string
     fmt.Scanln(&input)
